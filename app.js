ace34bb90c98ee07acca77a9eb060fad546126da
Prashant Yanpallewar <prashantyanpallewar172@gmail.com>
2025-09-05 16:17:42 +0530

    day 1

---
+++ app.js
@@ -0,0 +1,5 @@
+const element=React.createElement("h1",{},"hello prashant");
+//ReactDom.render(element,document.getElementById("root"));
+
+
+const Reactroot=ReactDom.createRoot(document.getElementById("root"));
